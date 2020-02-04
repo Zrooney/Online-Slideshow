bb84932994391a326ba0a9f9ef6749c6d7a4fa6d
Zachary Smeltz <zacho6000@gmail.com>
2020-02-03 21:39:37 -0500

    asd

--- main.ts
+++ main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//change in this shit its Monday my dude
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
